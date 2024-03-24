1bee8db45e885a440ced9b0f3d88604f57a1816e
ar-fe <f.arty00m@gmail.com>
2024-03-24 16:38:04 +0300

    Add files via upload

---
+++ cds-pattern/fit_restriction_site.js
@@ -0,0 +1,122 @@
+﻿function rev_comp(seq) {
+    const comp = {
+        'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
+        'R': 'Y', 'Y': 'R', 'S': 'S', 'W': 'W',
+        'K': 'M', 'M': 'K', 'B': 'V', 'V': 'B',
+        'D': 'H', 'H': 'D', 'N': 'N'
+    }
+    return seq.split('').reverse().map(c => comp[c]).join('')
+}
+
+const fwd_code = {
+    'TTT':   'F',  'TAT':   'Y',  'TCT':   'S',  'TGT':   'C',  'ATT':   'I',  'AAT':   'N',  'ACT':   'T',  'AGT':   'S',
+    'TTC':   'F',  'TAC':   'Y',  'TCC':   'S',  'TGC':   'C',  'ATC':   'I',  'AAC':   'N',  'ACC':   'T',  'AGC':   'S',
+    'TTA':   'L',  'TAA':   '*',  'TCA':   'S',  'TGA':   '*',  'ATA':   'I',  'AAA':   'K',  'ACA':   'T',  'AGA':   'R',
+    'TTG':   'L',  'TAG':   '*',  'TCG':   'S',  'TGG':   'W',  'ATG':   'M',  'AAG':   'K',  'ACG':   'T',  'AGG':   'R',
+    'CTT':   'L',  'CAT':   'H',  'CCT':   'P',  'CGT':   'R',  'GTT':   'V',  'GAT':   'D',  'GCT':   'A',  'GGT':   'G',
+    'CTC':   'L',  'CAC':   'H',  'CCC':   'P',  'CGC':   'R',  'GTC':   'V',  'GAC':   'D',  'GCC':   'A',  'GGC':   'G',
+    'CTA':   'L',  'CAA':   'Q',  'CCA':   'P',  'CGA':   'R',  'GTA':   'V',  'GAA':   'E',  'GCA':   'A',  'GGA':   'G',
+    'CTG':   'L',  'CAG':   'Q',  'CCG':   'P',  'CGG':   'R',  'GTG':   'V',  'GAG':   'E',  'GCG':   'A',  'GGG':   'G',
+}
+
+function translate(seq) {
+    return (seq.match(/.{3}/g) || []).map(codon => fwd_code[codon]).join('')
+}
+
+const rev_code = {
+    '*': ['TGA', 'TAA', 'TAG'], 'A': ['GCC', 'GCT', 'GCA', 'GCG'], 'M': ['ATG'], 'G': ['GGC', 'GGA', 'GGG', 'GGT'], 'S': ['AGC', 'TCC', 'TCT', 'TCA', 'AGT', 'TCG'],
+    'C': ['TGC', 'TGT'], 'N': ['AAT', 'AAC'], 'H': ['CAC', 'CAT'], 'T': ['ACC', 'ACA', 'ACT', 'ACG'], 'D': ['GAC', 'GAT'], 'P': ['CCT', 'CCC', 'CCA', 'CCG'],
+    'I': ['ATC', 'ATT', 'ATA'], 'V': ['GTG', 'GTC', 'GTT', 'GTA'],'E': ['GAG', 'GAA'], 'Q': ['CAG', 'CAA'], 'K': ['AAG', 'AAA'], 'W': ['TGG'], 'F': ['TTC', 'TTT'],
+    'R': ['AGA', 'AGG', 'CGG', 'CGC', 'CGA', 'CGT'], 'L': ['CTG', 'CTC', 'CTT', 'TTG', 'TTA', 'CTA'], 'Y': ['TAC', 'TAT']   		
+}
+
+function back_translate(seq) {
+    return seq.split('').map(aa => rev_code[aa][0]).join('')
+}
+
+function prot_to_deg(seq) {
+    const rev_code_deg = {
+        '*': 'TRR',
+        'A': 'GCN', 'M': 'ATG', 'G': 'GGN', 'S': 'WSN',
+        'C': 'TGY', 'N': 'AAY', 'H': 'CAY', 'T': 'ACN',
+        'D': 'GAY', 'P': 'CCN', 'I': 'ATH', 'V': 'GTN',
+        'E': 'GAR', 'Q': 'CAR', 'K': 'AAR', 'W': 'TGG',
+        'F': 'TTY', 'R': 'MGN', 'L': 'YTN', 'Y': 'TAY',
+    }
+    return seq.split('').map(a => rev_code_deg[a]).join('')
+}
+
+function pattern_to_prematch_re(pattern, add_rev_comp) {
+    const prematch_patterns = {
+        'A': '[ARWMDHVN]', 'G': '[GRSKBDVN]', 'C': '[CYSMBHVN]', 'T': '[TYWKBDHN]',
+        'R': '[AGRSWKMBDHVN]', 'Y': '[CTYSWKMBDHVN]', 'S': '[GCRYSKMBDHVN]', 'W': '[ATRYWKMBDHVN]', 
+        'K': '[GTRYSWKBDHVN]', 'M': '[ACRYSWMBDHVN]', 'B': '[GCTRYSWKMBDHVN]', 'D': '[AGTRYSWKMBDHVN]', 
+        'H': '[ACTRYSWKMBDHVN]', 'V': '[AGCRYSWKMBDHVN]', 'N': '[AGCTRYSWKMBDHVN]'
+    }    
+    function site_to_prematch(seq) {
+        return seq.split('').map(nt => prematch_patterns[nt]).join('')
+    }
+    let pattern_re = site_to_prematch(pattern)
+    if (add_rev_comp) {
+        const rc = rev_comp(pattern)
+        if (rc != pattern) pattern_re += '|' + site_to_prematch(rc)
+    }
+    return '(?=(' + pattern_re + '))'
+}
+
+function try_fit_prematch(prematch, seq) {
+    // prematch -- an original DNA seq with prematch place replaced by (possibly degenerate) RE site
+    // seq -- the original seq. both arguments are minimal length extended to full codons
+    const insert_patterns = {
+        'A': '[A]', 'G': '[G]', 'C': '[C]', 'T': '[T]', 
+        'R': '[AG]', 'Y': '[TC]', 'S': '[CG]', 'W': '[TA]',
+        'K': '[TG]', 'M': '[AC]', 'B': '[TCG]', 'D': '[TAG]',
+        'H': '[TAC]', 'V': '[ACG]', 'N': '[TACG]'
+    }
+    const m_codons = prematch.match(/.{3}/g)
+    const codons = seq.match(/.{3}/g)
+    let result_codons = []
+    loop: for (let i = 0; i < codons.length; i++) {
+        const m_c = m_codons[i]
+        const c = codons[i]
+        if (c == m_c) {
+            result_codons.push(c)
+            continue
+        }
+        const m_c_pattern = insert_patterns[m_c[0]] + insert_patterns[m_c[1]] + insert_patterns[m_c[2]]
+        if (c.match(m_c_pattern)) {
+            result_codons.push(c)
+            continue
+        }
+        for (const aa_c of rev_code[fwd_code[c]]) {
+            if (aa_c.match(m_c_pattern)) {
+                result_codons.push(aa_c)
+                continue loop
+            }
+        }
+        return null
+    }
+    return result_codons.join('')
+}
+
+function try_insert_pattern(seq, pattern, add_rev_comp) {
+    // seq -- DNA seq, must be 3*n in length
+    let fits = []
+    const deg_seq = prot_to_deg(translate(seq))
+    let prematches = deg_seq.matchAll(pattern_to_prematch_re(pattern, add_rev_comp))
+    for (m of prematches) {
+        const start = m.index - m.index % 3
+        const end = m.index + pattern.length + 1 + (3 - (m.index - start)) 
+        const subseq = seq.slice(start, end)
+        const prematch = subseq.substring(0, m.index - start) + pattern + subseq.substring(m.index - start + pattern.length)
+        let fit = try_fit_prematch(prematch, subseq)
+        let site_rc = rev_comp(pattern)
+        if (pattern != site_rc)
+            if (!fit)
+                fit = try_fit_prematch(subseq.substring(0, m.index - start) + site_rc + subseq.substring(m.index - start + pattern.length), subseq);
+        if (fit) {
+            fits.push({pos: m.index, seq: fit.substring(m.index - start, m.index - start + pattern.length)})
+        }
+    }
+    return fits
+}
---
+++ cds-pattern/re.js
@@ -0,0 +1,168 @@
+﻿
+function table_header(seq) {
+    const clustalX_colorscheme = {
+        '*': '#ffffff',
+        'A': '#80a0f0', 'D': '#c048c0', 'W': '#80a0f0', 'C': '#f08080',
+        'I': '#80a0f0', 'N': '#15c015', 'V': '#80a0f0', 'G': '#f09048',
+        'L': '#80a0f0', 'Q': '#15c015', 'K': '#f01505', 'P': '#c0c000',
+        'M': '#80a0f0', 'S': '#15c015', 'R': '#f01505', 'H': '#15a4a4',
+        'F': '#80a0f0', 'T': '#15c015', 'E': '#c048c0', 'Y': '#15a4a4',
+    }
+    let numbers = ''
+    let translated = translate(seq)
+    let start_pos = 1
+    for (let i = start_pos; i <= translated.length + start_pos - 1; i += 5) {
+        let s = String(i)
+        if (i % 10 == 5 || numbers == '') {
+            if (numbers == '') {
+                numbers = s
+                i -= i % 5
+            } else {
+                const to5 = '                . '
+                numbers += to5.substring(Math.max(to5.length - (i - start_pos + 1) * 3 + s.length % 15, s.length % 15))
+            }            
+        } else {
+            numbers += '               '.substring(0, Math.min((i - start_pos) * 3 - numbers.length % 15, 27 - numbers.length % 30))
+            if(numbers[numbers.length - 1] == ' ') numbers += s
+        }        
+    }
+    numbers = `<span style="color: gray; user-select: none;">${numbers}</span>`
+    let prot_seq = translated.split('')
+        .map(c => `<span style="background-color: ${clustalX_colorscheme[c]};"> ` + c + ' </span>').join('')
+    let tr = document.createElement('tr')
+    tr.innerHTML = '<th id="table-topleft">Click<br/>to →scroll:</th>' + 
+        '<th>' + numbers + '<br/>' + prot_seq + '<br/>' + seq + '</th>'
+    return tr
+}
+
+function result_tr(seq, re_name, fit) {
+    function highlight_site(seq, pos, site) {
+        return seq.substring(0, pos) +
+            '<span class="highlight">' +
+            site +
+            '</span>' +
+            seq.substring(pos + site.length)
+    }
+    let tr = document.createElement('tr')
+    tr.innerHTML = 
+        `<td><span class="action-link" onclick="scroll_to_match(this)" title="${restriction_enzymes[re_name]}">` + re_name + '</span></td>' +
+        '<td>' + highlight_site(seq, fit.pos, fit.seq) + '</td>'
+    return tr
+}
+
+function get_input_filtered(id, filter) {
+    let input = document.getElementById('in-seq').value.toUpperCase()
+    return input.split('').filter(c => filter.includes(c)).join('')
+}
+function get_input_cds(id) {
+    return get_input_filtered(id, 'AGTC')
+}
+function get_input_prot(id) {
+    return get_input_filtered(id, 'ACDEFGHIKLMNPQRSTVWY')
+}
+
+function calculate() {
+    let input_type = document.querySelector('input[name="input-type"]:checked').id
+    let cds = ''
+    if (input_type == 'in-prot-radio') {
+        const prot = get_input_prot('in-seq')
+        cds = back_translate(prot)
+    } else {
+        cds = get_input_cds('in-seq')
+        cds = cds.substring(0, cds.length - cds.length % 3)
+    }
+
+    let add_rev_comp = true
+    let patterns = {}
+    if (document.querySelector('#custom-seq-radio').checked) 
+    {
+        let patterns_in = document.querySelector('#custom-seq').value
+            .toUpperCase().split('\n')
+            .map(line => line.split('').filter(c => 'AGTCRYSWKMBVDHN'.includes(c)).join(''))
+            .filter(pattern => pattern != '')
+        patterns = Object.fromEntries(patterns_in.map(p => [p, p]))
+        add_rev_comp = false
+    } else {
+        // get selected REs
+        patterns = Object.fromEntries(Array.from(document.querySelectorAll('#restriction-enzymes input:checked')).map(el => [el.id, restriction_enzymes[el.id]]))
+    }
+    
+    let results_table = document.getElementById('results')
+    results_table.innerHTML = ''
+    results_table.appendChild(table_header(cds))
+
+    let found_seqs = []
+    for (const pattern_name of Object.keys(patterns)) {
+        fits = try_insert_pattern(cds, patterns[pattern_name], add_rev_comp)    
+        if (fits.length > 0 && !found_seqs.includes(patterns[pattern_name])) {
+            //add finds to table
+            fits.map(fit => result_tr(cds, pattern_name, fit)).forEach(tr => document.querySelector('table').appendChild(tr));
+            found_seqs.push(patterns[pattern_name])
+        }
+    }
+    if (found_seqs.length == 0) {
+        results_table.innerHTML += '<p style="color: red; position: sticky; left: 0;">Nothing found :(</p>'
+    }
+    results_table.scrollLeft = 0
+    results_table.scrollTop = 0
+}
+
+function rev_comp_input() {
+    document.getElementById('in-seq').value = rev_comp(get_input_cds())
+}
+
+let table = document.getElementById('restriction-enzymes')
+for (RE of Object.keys(restriction_enzymes)) {
+    let tr = document.createElement('tr')
+    tr.innerHTML = `<td><input type="checkbox" name="re-list" id="${RE}" ${subsets.sba.includes(RE) ? "checked" : ""}><label for="${RE}">${RE}</label></td><td style="font-family: monospace;">${restriction_enzymes[RE]}</td>`
+    table.appendChild(tr)
+}
+
+function scroll_to_match(el) {
+    let highlight = el.parentNode.parentNode.querySelector('.highlight')
+    document.getElementById('results').scrollLeft = highlight.offsetLeft
+}
+function select_subset(el) {
+    subsets[el.dataset.subset].forEach(
+        name => document.querySelector('#restriction-enzymes #' + name).checked = true
+    )
+}
+function deselect_subset(el) {
+    subsets[el.dataset.subset].forEach(name => document.querySelector('#restriction-enzymes #' + name).checked = false)
+}
+
+let crosshair = document.getElementById('crosshair')
+let crosshair_w = 0
+crosshair.style.width = crosshair_w + 'px'
+
+let drag_start = 0
+let dragging = false
+
+document.getElementById('results').addEventListener('mousemove', function (e) {
+    if (dragging) {
+        let left = Math.min(e.clientX, drag_start)
+        let right = Math.max(e.clientX, drag_start)
+        crosshair.style.left = left - crosshair_w / 2 + 'px'
+        crosshair.style.width = right - left + crosshair_w + 'px'
+    } else {
+        crosshair.style.left = e.clientX - crosshair_w / 2 + 'px'
+    }
+    crosshair.style.top = document.getElementById('results').offsetTop + 'px'
+    crosshair.style.height = document.getElementById('results').offsetHeight + 'px'
+})
+document.getElementById('results').addEventListener('mouseover', function (e) {
+    crosshair.style.display = 'block'
+})
+document.getElementById('results').addEventListener('mouseout', function (e) {
+    crosshair.style.display = 'none'
+})
+document.getElementById('results').addEventListener('mousedown', function (e) {
+    if (e.button != 0) return;
+    drag_start = e.clientX
+    dragging = true
+})
+document.getElementById('results').addEventListener('mouseup', function (e) {
+    if (e.button != 0) return;
+    dragging = false
+    crosshair.style.width = crosshair_w + 'px'
+})
